fb01b755c52a394d71f04cf122f9b2dc3697e162
Robert Miller <miller.robert.john@gmail.com>
2023-12-29 17:42:46 -0700

    Add contact page

--- main.go
+++ main.go
@@ -5,12 +5,21 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:miller.robert.john@gmail.com\">miller.robert.john@gmail.com</a>.")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
+
 	fmt.Println("Starting the server on :3000...")
+	
 	http.ListenAndServe(":3000", nil)
 }
