9f826f906059ff47d20ece4b674585c502b20050
Robert Miller <miller.robert.john@gmail.com>
2024-04-27 14:26:49 -0600

    More db experimentation

--- cmd/exp_02/main.go
+++ cmd/exp_02/main.go
@@ -9,15 +9,38 @@ import (
 	"github.com/joho/godotenv"
 )
 
+type PostgresConfig struct {
+	Host string
+	Port string
+	User string
+	Password string
+	DBName string
+	SSLMode string
+}
+
+func (cfg PostgresConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBName, cfg.SSLMode)
+}
+
 func main() {
 	// Load .env
 	err := godotenv.Load()
 	if err != nil {
 		panic(err)
 	}
+
+	// Postgres config
+	cfg := PostgresConfig{
+		Host: os.Getenv("POSTGRES_HOST"),
+		Port: os.Getenv("POSTGRES_PORT"),
+		User: os.Getenv("POSTGRES_USER"),
+		Password: os.Getenv("POSTGRES_PASSWORD"),
+		DBName: os.Getenv("POSTGRES_DBNAME"),
+		SSLMode: os.Getenv("POSTGRES_SSLMODE"),
+	}
 	
 	// Open db connection
-	db, err := sql.Open("pgx", os.Getenv("DATA_SOURCE_NAME"))
+	db, err := sql.Open("pgx", cfg.String())
 	if err != nil {
 		panic(err)
 	}
@@ -28,5 +51,37 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Connected!")
+	fmt.Println("Database connected.")
+
+	// Create tables
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS users (
+			id SERIAL PRIMARY KEY,
+			name TEXT,
+			email TEXT NOT NULL
+		);
+
+		CREATE TABLE IF NOT EXISTS orders (
+			id SERIAL PRIMARY KEY,
+			user_id INT NOT NULL,
+			amount INT,
+			description TEXT
+		);
+	`)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Tables created.")
+
+	// Insert user
+	name := "Robert Miller"
+	email := "miller.robert.john@gmail.com"
+	_, err = db.Exec(`
+		INSERT INTO users (name, email)
+		VALUES ($1, $2);
+	`, name, email)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("User created.")
 }
