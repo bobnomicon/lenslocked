537cc68c91501bd67fbc2e39236e7ceaa1059410
Robert Miller <miller.robert.john@gmail.com>
2025-01-29 21:43:17 -0700

    Add currentUser template func

--- views/template.go
+++ views/template.go
@@ -10,6 +10,8 @@ import (
 	"net/http"
 
 	"github.com/gorilla/csrf"
+	"github.com/operas-logicas/lenslocked/context"
+	"github.com/operas-logicas/lenslocked/models"
 )
 
 type Template struct {
@@ -30,6 +32,9 @@ func (t Template) Execute(w http.ResponseWriter, r *http.Request, data interface
 		template.FuncMap{
 			"csrfField": func() template.HTML {
 				return csrf.TemplateField(r)
+			},
+			"currentUser": func() *models.User {
+				return context.User(r.Context())
 			},
 		},
 	)
@@ -59,6 +64,9 @@ func ParseFS(fs fs.FS, pattern ...string) (Template, error) {
 		template.FuncMap{
 			"csrfField": func() (template.HTML, error) {
 				return "", fmt.Errorf("csrfField not implemented")
+			},
+			"currentUser": func() (*models.User, error) {
+				return nil, fmt.Errorf("currentUser not implemented")
 			},
 		},
 	)
